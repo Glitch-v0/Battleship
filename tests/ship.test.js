8ea7c318581c7f8c7d39c0313e7280a7fdb7f32a
Nathan Jones <njonessurvey@gmail.com>
2023-12-19 09:15:19 -0600

    remove ignored files from repo

--- tests/ship.test.js
+++
@@ -1,8 +0,0 @@
-import Ship from '../src/ship'
-
-test('new ship prperties successfully created', () => {
-    let carrier = new Ship(5)
-    expect(carrier.length).toBe(5)
-    expect(carrier.hits).toBe(0)
-    expect(carrier.sunk).toBe(false)
-})
